3143a9e0da83f1811ff485efd4aae8bd56de9408
t <git@example.com>
2026-02-06 15:08:46 -0500

    stop vimwiki from loading for every markdown file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -299,6 +299,8 @@ vim.g.vimwiki_list = {
   { path = "~/apps/vimwiki/database/" }, 
   { path = "~/apps/vimwiki/book/" }
 }
+-- stop Vimwiki from treating every markdown file as a wiki
+vim.g.vimwiki_global_ext = 0 
 
 
 -- idk why I gotta set this explicity, but I do!
