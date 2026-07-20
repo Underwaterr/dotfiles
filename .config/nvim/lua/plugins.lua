f3efe23241f4923b34bdca856d158ab66f4ad772
t <git@example.com>
2026-07-20 19:23:41 -0400

    add git gutter plugin

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -3,20 +3,21 @@
 local gh = function(x) return 'https://github.com/' .. x end
 
 vim.pack.add({
-  gh('nvim-lua/plenary.nvim'),                    -- lua utilities (telescope dependency)
-  gh('kevinhwang91/promise-async'),               -- ufo dependency
-  gh('nvim-tree/nvim-web-devicons'),              -- nerd fonts
-  gh('nvim-tree/nvim-tree.lua'),                  -- file explorer
-  gh('Mofiqul/dracula.nvim'),                     -- dracula theme
-  gh('nvim-lualine/lualine.nvim'),                -- status bar
-  gh('nvim-telescope/telescope.nvim'),            -- file search
-  gh('kevinhwang91/nvim-ufo'),                    -- code folding
-  gh('mason-org/mason.nvim'),                     -- LSP management
-  gh('saghen/blink.lib'),                         -- required by blink.cmp v2
-  gh('saghen/blink.cmp'),                         -- autocomplete
-  gh('lifepillar/pgsql.vim'),                     -- PostgreSQL syntax
-  gh('vimwiki/vimwiki'),                          -- VimWiki
-  gh('epwalsh/pomo.nvim'),                        -- pomodoro timer
+  gh('nvim-lua/plenary.nvim'),          -- lua utilities (telescope dependency)
+  gh('kevinhwang91/promise-async'),     -- ufo dependency
+  gh('nvim-tree/nvim-web-devicons'),    -- nerd fonts
+  gh('nvim-tree/nvim-tree.lua'),        -- file explorer
+  gh('Mofiqul/dracula.nvim'),           -- dracula theme
+  gh('nvim-lualine/lualine.nvim'),      -- status bar
+  gh('nvim-telescope/telescope.nvim'),  -- file search
+  gh('kevinhwang91/nvim-ufo'),          -- code folding
+  gh('mason-org/mason.nvim'),           -- LSP management
+  gh('saghen/blink.lib'),               -- required by blink.cmp v2
+  gh('saghen/blink.cmp'),               -- autocomplete
+  gh('lifepillar/pgsql.vim'),           -- PostgreSQL syntax
+  gh('vimwiki/vimwiki'),                -- VimWiki
+  gh('epwalsh/pomo.nvim'),              -- pomodoro timer
+  gh('lewis6991/gitsigns.nvim'),        -- git gutter
 
   -- MARKDOWN!!
   gh('OXY2DEV/markview.nvim'),
