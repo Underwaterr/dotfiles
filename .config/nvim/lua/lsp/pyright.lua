22072c43d1e3689f438de327750668a379de78f1
Underwaterr <3163892+Underwaterr@users.noreply.github.com>
2026-09-15 11:07:21 -0400

    add pyright config for nvim

---
+++ .config/nvim/lua/lsp/pyright.lua
@@ -0,0 +1,24 @@
+vim.lsp.config('pyright', {
+    cmd = { 'pyright-langserver', '--stdio' },
+    filetypes = { 'python' },
+    root_markers = {
+      'pyproject.toml',
+      'setup.py',
+      'setup.cfg',
+      'requirements.txt',
+      'pyrightconfig.json',
+      '.git'
+    },
+    settings = {
+      python = {
+        analysis = {
+          autoSearchPaths = true,
+          useLibraryCodeForTypes = true,
+          diagnosticMode = 'openFilesOnly',  -- 'workspace' is much heavier
+        }
+      }
+    }
+  }
+)
+
+vim.lsp.enable('pyright')
